2359ef05b79d7fe2724dfa56d19ccfbb5333c66d
Mateusz Biały <mateusz.bialy@pm.me>
2019-12-08 14:50:58 +0100

    Improved subscriptions management

--- subsmgr.py
+++ subsmgr.py
@@ -1,41 +1,33 @@
-import os
+from os import path
 
-path = os.path.dirname(__file__)
-subs_path = os.path.join(path, 'subscriptions.txt')
+subs_path = path.join(path.dirname(__file__), 'subscriptions.txt')
+
+#Create new subscriptions file if there isn't one yet
+if not path.exists(subs_path):
+    open(subs_path, 'w')
 
 #Returns subscribed channels IDs as a list of strings
 def get_subscribed_channels():
-    try:
-        subs_file = open(subs_path, 'r')
-    except IOError:
-        subs_file = open(subs_path, 'w') #Create new file if there isn't one
-        return []
-
-    #File to string list
-    subs = []
-    for line in subs_file:
-        subs.append(line)
-    subs_file.close()
-    return subs
+    with open(subs_path) as f:
+        return f.read().splitlines()
 
 #Returns true if subscriptions file contains channel_id, otherwise returns false
 def is_subscribed(channel_id):
-    return channel_id in open(subs_path).read()
+    with open(subs_path) as f:
+        return channel_id in f.read()
 
 #Retruns true if subscribed, false if unsubscribed
 def toggle_subscription(channel_id):
     if is_subscribed(channel_id):
-        #Delete line
-        f = open(subs_path, 'r')
-        lines = f.readlines()
-        f.close()
-        f = open(subs_path, 'w')
-        for line in lines:
-            if line != channel_id + '\n':
-                f.write(line)
-        f.close()
+        with open(subs_path) as f:
+            lines = f.read().splitlines()
+
+        with open(subs_path, 'w') as f:
+            for line in lines:
+                if line != channel_id:
+                    f.write(line + '\n')
         return False
+
     with open(subs_path, 'a') as file:
-        #Append line
         file.write(channel_id + '\n')
-        return True
+    return True
