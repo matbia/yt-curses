429ecc8e814e1a98ec84fbbdcf9cbac31a3eb892
Mateusz Biały <mateusz.bialy@pm.me>
2020-08-04 17:17:21 +0200

    Unify video parsing methods

--- client.py
+++ client.py
@@ -30,13 +30,9 @@ def send_request(url):
 def json_to_videos_list(data):
     videos = []
     for i in data:
-        videos.append(Video(i['videoId'], unescape(i['title']), str(datetime.timedelta(seconds=i['lengthSeconds'])), i['authorId'], i['author'], datetime.datetime.fromtimestamp(int(i['published']))))
-    return videos
-
-def json_to_recommended_videos_list(data):
-    videos = []
-    for i in data:
-        videos.append(Video(i['videoId'], unescape(i['title']), i['lengthSeconds'], i['authorId'], i['author'], ''))
+        vlen = str(datetime.timedelta(seconds=i['lengthSeconds']))
+        vpub = (lambda v: datetime.datetime.fromtimestamp(int(v['published'])) if 'published' in v else 'N/A in current mode')(i)
+        videos.append(Video(i['videoId'], unescape(i['title']), vlen, i['authorId'], i['author'], vpub))
     return videos
 
 def get_videos_from_channel(channel_id):
@@ -46,7 +42,7 @@ def search_videos(query):
     return json_to_videos_list(send_request(api_url + 'search?' + api_params + '&q=' + urllib.parse.quote_plus(query)))[::-1]
 
 def get_recommended_videos(video_id):
-    return json_to_recommended_videos_list(send_request(api_url + 'videos/' + video_id + '?fields=recommendedVideos')['recommendedVideos'])
+    return json_to_videos_list(send_request(api_url + 'videos/' + video_id + '?fields=recommendedVideos')['recommendedVideos'])
 
 def load_subscriptions_videos():
     videos = []
